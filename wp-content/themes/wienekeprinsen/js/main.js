9194127d36a5d01a12bac0fad16da0d5634adb10
kimsalabim <kimvaneerd@hotmail.com>
2016-09-19 21:25:17 +0200

    pricelist

--- wp-content/themes/wienekeprinsen/js/main.js
+++ wp-content/themes/wienekeprinsen/js/main.js
@@ -2,14 +2,15 @@ $(".priceTitle").click(function () {
 
     $header = $(this);
     //getting the next element
+    $min = $header.children(':first-child');
     $content = $header.next();
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
     $content.slideToggle(500, function () {
         //execute this after slideToggle is done
         //change text of header based on visibility of content div
-        $header.text(function () {
+        $min.text(function () {
             //change text based on condition
-            return $content.is(":visible") ? "Reflexologie" : "Geen Reflexologie";
+            return $content.is(":visible") ? "+" : "-";
         });
     });
 
