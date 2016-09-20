4bfa12a49c29d9f62dcd4fca8cd01b0e52f81333
kimsalabim <kimvaneerd@hotmail.com>
2016-09-20 17:32:14 +0200

    pricelist af

--- wp-content/themes/wienekeprinsen/js/main.js
+++ wp-content/themes/wienekeprinsen/js/main.js
@@ -4,25 +4,25 @@ $(".priceTitle").click(function () {
     if($header.css('background-color') == 'rgb(214, 238, 242)') {
                     $header.css('background-color','white');
                     $content.css('background-color','rgb(214,238,242)');
+                    $content.css('border-color','rgb(214,238,242)');
                 }
                 else {
                     $header.css('background-color','rgb(214,238,242)');
+                    $content.css('border-color','rgb(255,255,255)');
                     $content.css('background-color','rgb(255,255,255)');
                     
                 }
+     $min = $header.children(':first-child');
+            $min.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "-" : "+";
+        });
 
 
 
     //getting the next element
-    $min = $header.children(':first-child');
+   
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    $content.slideToggle(500, function () {
-                                    $min.text(function () {
-            //change text based on condition
-            return $content.is(":visible") ? "-" : "+";
-            
-        });
-                         
-                         });
+    $content.slideToggle(500);
 
 });
